1f40e0dd1b39559333b5e23023214fbfd97b686e
nishita goyal <goyalnishita07@gmail.com>
2018-07-11 13:52:56 +0530

    fifth commit

--- app/src/main/java/in/codingninjas/envision/expensemanager/CustomAdapter.java
+++ app/src/main/java/in/codingninjas/envision/expensemanager/CustomAdapter.java
@@ -1,6 +1,7 @@
 package in.codingninjas.envision.expensemanager;
 
 import android.content.Context;
+import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.ArrayAdapter;
@@ -12,11 +13,14 @@ public class CustomAdapter extends ArrayAdapter {
 
     Context mContext;
     ArrayList<Item> itemsList;
+    LayoutInflater inflater;
+
 
 
     // Constructor
     public CustomAdapter(Context context, ArrayList<Item> itemsList){
         super(context,0);
+        inflater = (LayoutInflater) context.getSystemService(Context.LAYOUT_INFLATER_SERVICE);
         mContext = context;
         this.itemsList = itemsList;
     }
@@ -24,21 +28,25 @@ public class CustomAdapter extends ArrayAdapter {
     // This function return the number of different type of views that will be there in the list view.
     @Override
     public int getViewTypeCount(){
+        return 2;
     }
 
     // This function returns the type of item(in our case header or list item) that adapter wants to know in getView function.
     @Override
     public int getItemViewType(int position) {
+        return itemsList.get(position).getItemType();
     }
 
     // This function gives the total count of items that will be there in the list.
     @Override
     public int getCount() {
+        return itemsList.size();
     }
 
     // This function returns the object of the itemList that has to inflated at that position.
     @Override
     public Object getItem(int position) {
+        return (itemsList.get(position));
     }
 
 
@@ -46,6 +54,8 @@ public class CustomAdapter extends ArrayAdapter {
     // we return the position, which is also unique for every item.
     @Override
     public long getItemId(int position) {
+
+        return position;
     }
 
 
@@ -56,7 +66,29 @@ public class CustomAdapter extends ArrayAdapter {
     @Override
     public View getView(int position, View convertView, ViewGroup parent) {
 
+        int type = itemsList.get(position).getItemType();
+        View output = null;
+        if (type == 0) {
+            output = inflater.inflate(R.layout.header_item_layout, parent, false);
+            TextView tv1 = output.findViewById(R.id.headerTitleTextView);
+            HeaderItem header = (HeaderItem) itemsList.get(position);
+            tv1.setText(header.getHeaderTitle());
+
+        } else if (type == 1) {
+            output = inflater.inflate(R.layout.expense_row_layout, parent, false);
+            TextView tv1 = output.findViewById(R.id.expenseName);
+            TextView tv2 = output.findViewById(R.id.expenseAmount);
+            TextView tv3 = output.findViewById(R.id.expenseDate);
+            TextView tv4 = output.findViewById(R.id.expenseTime);
+            Expense expense = (Expense) itemsList.get(position);
+            tv1.setText(expense.getName());
+            tv2.setText(expense.getAmount() + "");
+            tv3.setText(expense.getDate());
+            tv4.setText(expense.getTime());
+        }
+        return output;
     }
+        
 
 
 }
